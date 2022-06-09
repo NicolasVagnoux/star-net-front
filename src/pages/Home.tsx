84eafe87016103db5f48ec84aa7a8096feb5c7e1
AntsoTech <antso-rakotoson@hotmail.fr>
2022-06-09 17:48:32 +0200

    followed buttons ok

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import FollowedButton from '../components/FollowedButton';
 
 const Home = () => {
   return (
     <div>
       <h1>HELLO HOME</h1>
+      <FollowedButton />
+      <FollowedButton />
     </div>
   );
 };
