d1a515657c6bfe39ee696e5ef2c32709dd19225c
AntsoTech <antso-rakotoson@hotmail.fr>
2022-06-11 14:30:00 +0200

    buttons ok

---
+++ src/components/BadgesButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
+
+const BadgesButton = () => {
+  return (
+    <div className="badgesbutton">
+      <WorkspacePremiumIcon />
+    </div>
+  );
+};
+
+export default BadgesButton;
