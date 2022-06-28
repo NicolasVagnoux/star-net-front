ca64929386b13c57117015b8386c02f45f68f555
stefankolpacoff <stefan.kolpacoff@gmail.com>
2022-06-28 14:58:54 +0200

    updated title bar / changed followed button colors / added circular completion chart

--- src/components/CompletionChart.tsx
+++ src/components/CompletionChart.tsx
@@ -1,7 +1,29 @@
+import 'react-circular-progressbar/dist/styles.css';
+
 import React from 'react';
+import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
 
 const CompletionChart = () => {
-  return <div className="completionchart"> 25%</div>;
+  const value = 66;
+
+  return (
+    <div className="completionchart">
+      <CircularProgressbar
+        value={value}
+        text={`${value}%`}
+        strokeWidth={15}
+        // background
+        backgroundPadding={10}
+        styles={buildStyles({
+          // backgroundColor: '#3e98c7',
+          textColor: '#primaryBlack',
+          textSize: '30px',
+          pathColor: '#secondaryColor5',
+          trailColor: '#secondaryColor2',
+        })}
+      />
+    </div>
+  );
 };
 
 export default CompletionChart;
